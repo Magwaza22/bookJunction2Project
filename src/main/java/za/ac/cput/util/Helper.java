9cedb2309f1fdea9c374c02cfd608fdfd6602a85
Magwaza22 <127666759+Magwaza22@users.noreply.github.com>
2024-08-09 14:33:38 +0200

    Author, Helper, and Main class completed

--- src/main/java/za/ac/cput/util/Helper.java
+++ src/main/java/za/ac/cput/util/Helper.java
@@ -1,4 +1,21 @@
 package za.ac.cput.util;
 
+import java.util.UUID;
+
 public class Helper {
+
+    public static boolean isNullOrEmpty(String s){
+        if (s == null|| s.isEmpty())
+            return true;
+        return false;
+    }
+    public static String generatedID() {return UUID.randomUUID().toString();}
+
+    public static boolean assertInRange(int i) {
+        if (i >= 1 && i <= 5) {
+            return true;
+        }
+        return false;
+    }
+
 }
