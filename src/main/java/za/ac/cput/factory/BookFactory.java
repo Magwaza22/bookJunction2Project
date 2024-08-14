9b742be021524440349a022fc3bdf7a12bbeff55
Magwaza22 <127666759+Magwaza22@users.noreply.github.com>
2024-08-14 17:20:54 +0200

    Book factory class completed

--- src/main/java/za/ac/cput/factory/BookFactory.java
+++ src/main/java/za/ac/cput/factory/BookFactory.java
@@ -1,5 +1,25 @@
 package za.ac.cput.factory;
 
-public class BookFactory {
+import za.ac.cput.domain.Author;
+import za.ac.cput.domain.Book;
+import za.ac.cput.util.Helper;
 
+import java.util.Arrays;
+
+public class BookFactory {
+    public static Book createBook(Long bookID , byte[] bookPhoto, String ISBN, String title, String edition, Author author, Double price) {
+        if (Helper.isNullOrEmpty(String.valueOf(bookID)) || Helper.isNullOrEmpty(Arrays.toString((bookPhoto)))
+                || Helper.isNullOrEmpty((ISBN)) || Helper.isNullOrEmpty((title)) || Helper.isNullOrEmpty((edition))
+                || Helper.isNullOrEmpty(String.valueOf((author))) || Helper.isNullOrEmpty(String.valueOf((price))) )
+            return null;
+
+        return new Book.Builder().setBookID(bookID)
+                .setBookPhoto(bookPhoto)
+                .setISBN(ISBN)
+                .setTitle(title)
+                .setEdition(edition)
+                .setAuthor(author)
+                .setPrice(price)
+                .build();
+    }
 }
