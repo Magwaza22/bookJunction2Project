849544a419f3f7a877d8189e33a223488ffff4b1
Magwaza22 <127666759+Magwaza22@users.noreply.github.com>
2024-08-25 21:22:43 +0200

    Book Controller test package setup

---
+++ src/test/java/za/ac/cput/controller/BookControllerTest.java
@@ -0,0 +1,29 @@
+package za.ac.cput.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void read() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+
+    @Test
+    void getAll() {
+    }
+}
