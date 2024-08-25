6df5359138ee4c99772437463193c768922e38bb
Magwaza22 <127666759+Magwaza22@users.noreply.github.com>
2024-08-25 21:19:29 +0200

    Book Controller completed

--- src/main/java/za/ac/cput/controller/BookController.java
+++ src/main/java/za/ac/cput/controller/BookController.java
@@ -1,9 +1,36 @@
 package za.ac.cput.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import za.ac.cput.domain.Book;
+import za.ac.cput.service.BookService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/book")
 public class BookController {
+
+    private BookService service;
+
+    @Autowired
+    BookController(BookService service) { this.service = service; }
+
+    @PostMapping("/save")
+    public Book save(@RequestBody Book book) {
+        return service.create(book);
+    }
+
+    @GetMapping("/read/{id}")
+    public Book read(@PathVariable String id) {
+        return service.read(Long.valueOf(id));
+    }
+
+    @DeleteMapping("/deletebyid/{id}")
+    public void deleteById(@PathVariable String id) {
+        service.delete(Long.valueOf(id));
+    }
+
+    @GetMapping("/getall")
+    public List<Book> getAll() { return service.getAll(); }
 }
