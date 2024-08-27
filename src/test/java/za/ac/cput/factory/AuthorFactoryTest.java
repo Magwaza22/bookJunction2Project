a2ff60e64f64eee3d80b3859be3d54c8acabb060
Magwaza22 <127666759+Magwaza22@users.noreply.github.com>
2024-08-27 23:11:23 +0200

    Book and author factory tests modified as requested

--- src/test/java/za/ac/cput/factory/AuthorFactoryTest.java
+++ src/test/java/za/ac/cput/factory/AuthorFactoryTest.java
@@ -1,12 +1,21 @@
 package za.ac.cput.factory;
 
 import org.junit.jupiter.api.Test;
+import za.ac.cput.domain.Author;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class AuthorFactoryTest {
+public class AuthorFactoryTest {
 
     @Test
-    void createAuthor() {
+    public void testCreateAuthor_Successful() {
+        Author author = AuthorFactory.createAuthor("J.K.", "Rowling","rowling@pub.org",  "0748274967", "American");
+        assertNotNull(author);
+        assertEquals("J.K.", author.getFirstName());
+        assertEquals("Rowling", author.getLastName());
+        assertEquals("rowling@pub.org", author.getEmail());
+        assertEquals("0748274967", author.getPhoneNumber());
+        assertEquals("American", author.getNationality());
     }
+
 }
--- src/test/java/za/ac/cput/factory/BookFactoryTest.java
+++ src/test/java/za/ac/cput/factory/BookFactoryTest.java
@@ -1,16 +1,43 @@
 package za.ac.cput.factory;
 
 import org.junit.jupiter.api.Test;
+import za.ac.cput.domain.Book;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class BookFactoryTest {
+public class BookFactoryTest {
 
     @Test
-    void createBook() {
-    }
+    public void testCreateBook_Successful() {
+        byte[] bookPhoto = new byte[]{1, 2, 3}; // Example byte array for the book photo
 
-    @Test
-    void buildBook() {
+        Book book = BookFactory.createBook(
+                1L,
+                bookPhoto,
+                "978-3-16-148410-0",
+                "Harry Potter",
+                "First Edition",
+                "J.K.",
+                "Rowling",
+                "jk.rowling@example.com",
+                "123-456-7890",
+                "British",
+                29.99
+        );
+
+        assertNotNull(book);
+        assertEquals(1L, book.getBookID());
+        assertArrayEquals(bookPhoto, book.getBookPhoto());
+        assertEquals("978-3-16-148410-0", book.getISBN());
+        assertEquals("Harry Potter", book.getTitle());
+        assertEquals("First Edition", book.getEdition());
+        assertNotNull(book.getAuthor());
+        assertEquals("J.K.", book.getAuthor().getFirstName());
+        assertEquals("Rowling", book.getAuthor().getLastName());
+        assertEquals("jk.rowling@example.com", book.getAuthor().getEmail());
+        assertEquals("123-456-7890", book.getAuthor().getPhoneNumber());
+        assertEquals("British", book.getAuthor().getNationality());
+        assertEquals(29.99, book.getPrice());
     }
 }
+
