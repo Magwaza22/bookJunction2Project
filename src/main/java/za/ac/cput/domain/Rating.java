1b2465a39358cee19628b4fd0a59daa2e280e1be
Breezieee <221427953@mycput.ac.za>
2024-08-29 13:12:57 +0200

    New classes iv

--- src/main/java/za/ac/cput/domain/Rating.java
+++ src/main/java/za/ac/cput/domain/Rating.java
@@ -2,29 +2,37 @@ package za.ac.cput.domain;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import java.util.Date;
 
 import java.util.Objects;
 @Entity
 public class Rating {
     @Id
+    private Long id;
     private int overAllRating;
     private int bookConditionRating;
     private int sellerReliabilityRating;
     private int valueRating;
     private int deliveryRating;
     private int accuracyOfRating;
+    private Date date;
+    private User user;
 
     public Rating(){}
 
     private Rating(Builder builder){
+        id = builder.id;
         overAllRating = builder.overAllRating;
         bookConditionRating = builder.bookConditionRating;
         sellerReliabilityRating = builder.sellerReliabilityRating;
         valueRating = builder.valueRating;
         deliveryRating = builder.deliveryRating;
         accuracyOfRating = builder.accuracyOfRating;
+        date = builder.date;
 
     }
+
+    public Long getId() {return id;}
 
     public int getOverAllRating() {
         return overAllRating;
@@ -50,37 +58,49 @@ public class Rating {
         return accuracyOfRating;
     }
 
+    public Date getDate() {return date;}
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Rating rating = (Rating) o;
-        return overAllRating == rating.overAllRating && bookConditionRating == rating.bookConditionRating && sellerReliabilityRating == rating.sellerReliabilityRating && valueRating == rating.valueRating && deliveryRating == rating.deliveryRating && accuracyOfRating == rating.accuracyOfRating;
+        return overAllRating == rating.overAllRating && bookConditionRating == rating.bookConditionRating && sellerReliabilityRating == rating.sellerReliabilityRating && valueRating == rating.valueRating && deliveryRating == rating.deliveryRating && accuracyOfRating == rating.accuracyOfRating && Objects.equals(id, rating.id) && Objects.equals(date, rating.date);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(overAllRating, bookConditionRating, sellerReliabilityRating, valueRating, deliveryRating, accuracyOfRating);
+        return Objects.hash(id, overAllRating, bookConditionRating, sellerReliabilityRating, valueRating, deliveryRating, accuracyOfRating, date);
     }
 
     @Override
     public String toString() {
         return "Rating{" +
-                "overAllRating=" + overAllRating +
+                "id=" + id +
+                ", overAllRating=" + overAllRating +
                 ", bookConditionRating=" + bookConditionRating +
                 ", sellerReliabilityRating=" + sellerReliabilityRating +
                 ", valueRating=" + valueRating +
                 ", deliveryRating=" + deliveryRating +
                 ", accuracyOfRating=" + accuracyOfRating +
+                ", date=" + date +
                 '}';
     }
+
     public static class Builder{
+        private Long id;
         private int overAllRating;
         private int bookConditionRating;
         private int sellerReliabilityRating;
         private int valueRating;
         private int deliveryRating;
         private int accuracyOfRating;
+        private Date date;
+
+        public Builder setId(Long id) {
+            this.id = id;
+            return this;
+        }
 
         public Builder setOverAllRating(int overAllRating) {
             this.overAllRating = overAllRating;
@@ -109,6 +129,11 @@ public class Rating {
 
         public Builder setAccuracyOfRating(int accuracyOfRating) {
             this.accuracyOfRating = accuracyOfRating;
+            return this;
+        }
+
+        public Builder setDate(Date date) {
+            this.date = date;
             return this;
         }
 
---
+++ src/main/java/za/ac/cput/factory/RatingFactory.java
@@ -0,0 +1,28 @@
+package za.ac.cput.factory;
+
+
+import za.ac.cput.domain.Rating;
+import za.ac.cput.util.Helper;
+
+public class RatingFactory {
+
+    public static Rating rateBuild(Long id, int overAllRating, int bookConditionRating, int sellerReliabilityRating, int valueRating, int deliveryRating, int accuracyOfRating){
+        if (Helper.assertInRange(overAllRating) ||
+                Helper.assertInRange(bookConditionRating) ||
+                Helper.assertInRange(sellerReliabilityRating) ||
+                Helper.assertInRange(valueRating) ||
+                Helper.assertInRange(deliveryRating) ||
+                Helper.assertInRange(accuracyOfRating))
+            return null;
+
+        return new Rating.Builder()
+                .setBookConditionRating(bookConditionRating)
+                .setSellerReliabilityRating(sellerReliabilityRating)
+                .setValueRating(valueRating)
+                .setDeliveryRating(deliveryRating)
+                .setAccuracyOfRating(accuracyOfRating)
+                .setOverAllRating(overAllRating)
+                .build();
+
+    }
+}
