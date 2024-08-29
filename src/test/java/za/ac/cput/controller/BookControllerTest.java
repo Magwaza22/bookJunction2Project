e4b72faeebd0efd7af11b3da5d6fde201b15827b
Magwaza22 <127666759+Magwaza22@users.noreply.github.com>
2024-08-29 17:49:20 +0200

    controller and service test

--- src/test/java/za/ac/cput/controller/BookControllerTest.java
+++ src/test/java/za/ac/cput/controller/BookControllerTest.java
@@ -1,16 +1,18 @@
 package za.ac.cput.controller;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import za.ac.cput.domain.Book;
 import za.ac.cput.factory.BookFactory;
 
+import java.net.http.HttpHeaders;
+
+import static com.fasterxml.jackson.databind.type.LogicalType.Map;
 import static org.junit.jupiter.api.Assertions.*;
 
 @TestMethodOrder(MethodOrderer.MethodName.class)
@@ -19,11 +21,11 @@ class BookControllerTest {
 
     @Autowired
     private TestRestTemplate restTemplate;
-    private final String BASE_URL = "http://localhost:8080/book_junction/book";
+    private final String BASE_URL = "http://localhost:8080/bookjunction2Project/book";
     private static Book book;
 
-    @BeforeEach
-    void setUp() {
+    @BeforeAll
+    public static void setUp() {
         byte[] bookPhoto = new byte[]{1, 2, 3};
         book = BookFactory.createBook(3L, bookPhoto, "267-3-16-28861-2", "Java Programming", "Third Edition",
                 "Ted", "James", "jamesT@example.com", "089-289-6729",
@@ -44,14 +46,30 @@ class BookControllerTest {
 
     @Test
     void read() {
+        String url = BASE_URL + "/read/" + book.getBookID();
+        System.out.println("URL:" + url);
+        ResponseEntity<Book> response = restTemplate.getForEntity(url, Book.class);
+        assertEquals(book.getBookID(), response.getBody().getBookID());
+        System.out.println("Read: " + response.getBody());
 
     }
 
     @Test
     void deleteByID() {
+        String url = BASE_URL + "/delete/" + book.getBookID();
+        System.out.println("URL: " + url);
+        restTemplate.delete(url);
+        System.out.println("Deleted book");
     }
 
     @Test
     void getAll() {
+       /* String url = BASE_URL + "/getall";
+        HttpHeaders headers = new HttpHeaders();
+        HttpEntity<String> entity = new HttpEntity<>(null,headers);
+        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
+        System.out.println("Show All: ");
+        System.out.println(response);
+        System.out.println(response.getBody());*/
     }
 }
---
+++ src/test/java/za/ac/cput/service/SellerServiceTest.java
@@ -0,0 +1,85 @@
+package za.ac.cput.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import za.ac.cput.domain.Seller;
+import za.ac.cput.repository.SellerRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
+
+class SellerServiceTest {
+
+    @InjectMocks
+    private SellerService sellerService;
+
+    @Mock
+    private SellerRepository sellerRepository;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void testCreateSeller() {
+        Seller seller = new Seller();
+        when(sellerRepository.save(any(Seller.class))).thenReturn(seller);
+
+        Seller createdSeller = sellerService.create(seller);
+        assertNotNull(createdSeller);
+    }
+
+    @Test
+    void testReadSeller() {
+        Seller seller = new Seller();
+        when(sellerRepository.findById(String.valueOf(anyLong()))).thenReturn(Optional.of(seller));
+
+        Seller foundSeller = sellerService.read(1L);
+        //assertNotNull(foundSeller);
+    }
+
+    @Test
+    void testReadSeller_NotFound() {
+        when(sellerRepository.findById(String.valueOf(anyLong()))).thenReturn(Optional.empty());
+
+        Seller foundSeller = sellerService.read(1L);
+        assertNull(foundSeller);
+    }
+
+    @Test
+    void testUpdateSeller() {
+        Seller seller = new Seller();
+        when(sellerRepository.save(any(Seller.class))).thenReturn(seller);
+
+        Seller updatedSeller = sellerService.update(seller);
+        assertNotNull(updatedSeller);
+    }
+
+    @Test
+    void testDeleteSeller() {
+        doNothing().when(sellerRepository).deleteById(String.valueOf(anyLong()));
+
+        assertDoesNotThrow(() -> sellerService.delete(1L));
+        verify(sellerRepository, times(1)).deleteById(String.valueOf(1L));
+    }
+
+    @Test
+    void testGetAllSellers() {
+        List<Seller> sellers = new ArrayList<>();
+        when(sellerRepository.findAll()).thenReturn(sellers);
+
+        List<Seller> foundSellers = sellerService.getAll();
+        assertEquals(sellers, foundSellers);
+    }
+
+}
