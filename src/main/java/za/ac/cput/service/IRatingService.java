4bcba280c3fd89e1a4d46f4a35c2712a22400a61
Breezieee <221427953@mycput.ac.za>
2024-08-29 15:50:41 +0200

    New classes v

---
+++ src/main/java/za/ac/cput/service/IRatingService.java
@@ -0,0 +1,16 @@
+package za.ac.cput.service;
+
+import org.springframework.stereotype.Service;
+import za.ac.cput.domain.Rating;
+import za.ac.cput.domain.User;
+
+import java.util.List;
+
+@Service
+public interface IRatingService extends IService<Rating, Long>{
+
+    List getAll(Rating id);
+
+    void FindRatingByUser(User id);
+
+}
