cd43007931f1c02487a18db00a6490f357521a86
Ngcukana <221243526@mycput.ac.za>
2024-08-30 23:08:42 +0200

    Please find my controllers, Indi.

---
+++ src/main/java/za/ac/cput/controller/TransactionHistoryController.java
@@ -0,0 +1,22 @@
+package za.ac.cput.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import za.ac.cput.domain.TransactionHistory;
+import za.ac.cput.service.TransactionHistoryService;
+
+@RestController
+@RequestMapping("/transactionHistory")
+public class TransactionHistoryController {
+
+    private final TransactionHistoryService service;
+
+    @Autowired
+    TransactionHistoryController(TransactionHistoryService service){ this.service = service; }
+
+    @PostMapping("/save")
+    public TransactionHistory save(@RequestBody TransactionHistory transactionHistory){ return service.create(transactionHistory);}
+
+    @GetMapping("/read")
+    public TransactionHistory read(@PathVariable String id) { return service.read(id);}
+}
