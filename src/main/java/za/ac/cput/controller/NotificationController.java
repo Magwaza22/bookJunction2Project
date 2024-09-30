8f0ad79f7cf3bd0e0b07c861fdb8bd298560f0e1
Zisanda <217207715@mycput.ac.za>
2024-09-30 10:05:47 +0200

    CONTROLLER

---
+++ src/main/java/za/ac/cput/controller/NotificationController.java
@@ -0,0 +1,34 @@
+package za.ac.cput.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import za.ac.cput.domain.Notification;
+
+import za.ac.cput.service.BookService;
+import za.ac.cput.service.NotificationService;
+
+import java.util.List;
+
+public class NotificationController {
+    private final NotificationService service;
+
+    @Autowired
+    NotificationController(NotificationService service) { this.service = service; }
+
+    @PostMapping("/save")
+    public Notification save(@RequestBody Notification notification) {
+        return service.create(notification);
+    }
+
+    @GetMapping("/read/{id}")
+    public Notification read(@PathVariable String id) {
+        return service.read(Integer.valueOf(id));
+    }
+
+    @DeleteMapping("/deletebyid/{id}")
+    public void deleteByID(@PathVariable String id){service.delete(Integer.valueOf(id));}
+
+    @GetMapping("/getall")
+    public List<Notification> getAll(){return service.getall();}
+}
